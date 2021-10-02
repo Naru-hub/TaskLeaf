34245f726296802ed6d0ba488fd4da646627df66
Naru-hub <1208ny0324@gmail.com>
2021-10-02 12:02:07 +0000

    add administrator column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_124529) do
+ActiveRecord::Schema.define(version: 2021_10_02_115646) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_124529) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
